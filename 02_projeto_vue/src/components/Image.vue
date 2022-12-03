8ab2b5a43a0c05c5137198310a66ddcf86d0a7be
ricardo.rocker <ricardo.santos.rocker@gmail.com>
2022-12-03 02:15:30 -0300

    Reutilização de componentes

--- 02_projeto_vue/src/components/Image.vue
+++ 02_projeto_vue/src/components/Image.vue
@@ -1,15 +1,21 @@
 <template>
-    <img :src="sourceImage" :alt="descriptionImage" style="max-width:100px">
+  <div>
+    <img :src="sourceImage" :alt="descriptionImage" style="max-width: 100px" />
+    <Reutilizacao />
+  </div>
 </template>
 
 <script>
+import Reutilizacao from "./Reutilizacao.vue";
+
 export default {
-    name: 'Image',
-    data() {
-        return {
-            sourceImage: '/img/gorro.png',
-            descriptionImage: 'Minha imagem!'
-        }
-    }
-}
+  name: "Image",
+  data() {
+    return {
+      sourceImage: "/img/gorro.png",
+      descriptionImage: "Minha imagem!",
+    };
+  },
+  components: { Reutilizacao },
+};
 </script>
