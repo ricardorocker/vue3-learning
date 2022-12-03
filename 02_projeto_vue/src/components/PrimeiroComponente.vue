e9dde3e705d1d707994decd2be405c441bf96e2d
ricardo.rocker <ricardo.santos.rocker@gmail.com>
2022-12-03 01:54:49 -0300

    Múltiplos eventos

--- 02_projeto_vue/src/components/PrimeiroComponente.vue
+++ 02_projeto_vue/src/components/PrimeiroComponente.vue
@@ -2,11 +2,14 @@
     <div>
         <h1>Olá Vue!</h1>
         <p>Meu nome é {{ name }} e moro em {{ city }}</p>
+        <MultiplosEventos />
     </div>
 </template>
 
 <script>
+import MultiplosEventos from './MultiplosEventos.vue'
 export default {
+  components: { MultiplosEventos },
     name: 'PrimeiroComponente',
     data() {
         return {
