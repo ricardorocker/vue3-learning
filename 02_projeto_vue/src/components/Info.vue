186400248e23984d63d9b73abae30bb715485229
ricardo.rocker <ricardo.santos.rocker@gmail.com>
2022-12-02 21:36:21 -0300

    Diretivas (v-if, v-show)

--- 02_projeto_vue/src/components/Info.vue
+++ 02_projeto_vue/src/components/Info.vue
@@ -1,17 +1,26 @@
 <template>
     <div>
-        <p>Estou trabalhando no momento</p>
+        <p v-if="isWorking">Estou trabalhando no momento</p>
+        <p v-else>Estou em busca de novas oportunidades!</p>
         <p>Utilizo as seguintes tecnologias:</p>
         <ul>
             <li>Javascript</li>
             <li>PHP</li>
             <li>Python</li>
         </ul>
+        <p v-show="showEmail">Mande uma mensagem para: {{email}}</p>
     </div>
 </template>
 
 <script>
 export default {
-    name: 'Info'
+    name: 'Info',
+    data() {
+        return {
+            isWorking: false,
+            showEmail: true,
+            email: 'ricardo.santos.rocker@gmail.com',
+        }
+    }
 }
 </script>
