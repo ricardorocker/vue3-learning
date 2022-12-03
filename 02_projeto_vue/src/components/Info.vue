b3d6e0006561a521bac4192aefd8196e544cf3c1
ricardo.rocker <ricardo.santos.rocker@gmail.com>
2022-12-02 22:07:16 -0300

    Argumentos

--- 02_projeto_vue/src/components/Info.vue
+++ 02_projeto_vue/src/components/Info.vue
@@ -8,18 +8,23 @@
             <li>PHP</li>
             <li>Python</li>
         </ul>
-        <p v-show="showEmail">Mande uma mensagem para: {{email}}</p>
+        <p v-show="showEmail">Mande uma mensagem para: {{ email }}</p>
+        <p>Para acessar meu portfólio basta <a v-bind:href="urlPortfolio" target="blank">clicar aqui</a></p>
+        <Image />
     </div>
 </template>
 
 <script>
+import Image from './Image.vue'
 export default {
+    components: { Image },
     name: 'Info',
     data() {
         return {
             isWorking: false,
             showEmail: true,
             email: 'ricardo.santos.rocker@gmail.com',
+            urlPortfolio: 'http://www.google.com.br/'
         }
     }
 }
